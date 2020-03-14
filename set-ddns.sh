7c2dfbb3e3d1a64ae423803eeaac4174e0e5fbb5
chinabjy <48501377+chinabjy@users.noreply.github.com>
2020-03-14 15:38:46 +0800

    Update set-ddns.sh

--- set-ddns.sh
+++ set-ddns.sh
@@ -19,12 +19,18 @@ cd
 echo "正在安装依赖...."
 yum install -y wget bind-utils &> /dev/null
 cd /usr/local
-rm -f /usr/local/iptables4ddns.sh
-wget https://raw.githubusercontent.com/chinabjy/iptablesUtils/ddns-check.sh  &> /dev/null
+rm -f /usr/local/ddns-check.sh
+wget https://raw.githubusercontent.com/chinabjy/iptablesUtils/master/ddns-check.sh  &> /dev/null
 chmod +x /usr/local/ddns-check.sh
 echo "Done!"
 echo ""
 
+local=$(ip -o -4 addr list | grep -Ev '\s(docker|lo)' | awk '{print $4}' | cut -d/ -f1 | grep -Ev '(^127\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$)|(^10\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$)|(^172\.1[6-9]{1}[0-9]{0,1}\.[0-9]{1,3}\.[0-9]{1,3}$)|(^172\.2[0-9]{1}[0-9]{0,1}\.[0-9]{1,3}\.[0-9]{1,3}$)|(^172\.3[0-1]{1}[0-9]{0,1}\.[0-9]{1,3}\.[0-9]{1,3}$)|(^192\.168\.[0-9]{1,3}\.[0-9]{1,3}$)')
+if [ "${local}" = "" ]; then
+	local=$(ip -o -4 addr list | grep -Ev '\s(docker|lo)' | awk '{print $4}' | cut -d/ -f1 )
+fi
+echo  local-ip: $local
+echo  重新设置iptables转发
 
 echo -n "本地端口号:" ;read localport
 echo -n "远程端口号:" ;read remoteport
