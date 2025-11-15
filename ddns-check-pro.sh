bf38aca977d356b7cc8e526c13b4ed173fcb4fda
chinabjy <48501377+chinabjy@users.noreply.github.com>
2025-11-15 19:28:28 +0800

    Update ddns-check-pro.sh

--- ddns-check-pro.sh
+++ ddns-check-pro.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-# 暂停范围：0.000–3.000 秒
-sleep "$(awk 'BEGIN{srand(); printf "%.3f", rand()*3}')"
+# 暂停范围：0.000–10.000 秒
+sleep "$(awk 'BEGIN{srand(); printf "%.3f", rand()*10}')"
+
 
 # 颜色定义
 RED="\033[31m"
