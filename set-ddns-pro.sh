e96381d5851c5916abd094b217f52be8b31a7cf2
chinabjy <48501377+chinabjy@users.noreply.github.com>
2026-03-17 19:16:55 +0800

    Update set-ddns-pro.sh

--- set-ddns-pro.sh
+++ set-ddns-pro.sh
@@ -62,14 +62,15 @@ IPrecordfile=${localport}[${targetDDNS}:${remoteport}]
 chmod +x /etc/rc.d/rc.local
 echo "rm -f /root/$IPrecordfile" >> /etc/rc.d/rc.local
 # 替换下面的localport remoteport targetDDNS
-echo "/bin/bash /usr/local/ddns-check.sh $localport $remoteport $targetDDNS ${localport}[${targetDDNS}:${remoteport}] $localip $allowed_source &>> /root/iptables${localport}.log" >> /etc/rc.d/rc.local
+echo "/bin/bash /usr/local/ddns-check-pro.sh $localport $remoteport $targetDDNS ${localport}[${targetDDNS}:${remoteport}] $localip $allowed_source &>> /root/iptables${localport}.log" >> /etc/rc.d/rc.local
 chmod +x /etc/rc.d/rc.local
 # 定时任务，每分钟检查一下
 # 修改crontab任务行，添加最后一个参数 $allowed_source
+
 echo "* * * * * root sleep $(( RANDOM % 30 ));  /usr/local/ddns-check-pro.sh $localport $remoteport $targetDDNS ${localport}[${targetDDNS}:${remoteport}] $localip $allowed_source &>> /root/iptables${localport}.log" >> /etc/crontab
 #echo "* * * * * root /usr/local/ddns-check-pro.sh $localport $remoteport $targetDDNS $IPrecordfile $localip &>> /root/iptables${localport}.log" >> /etc/crontab
 cd
 rm -f /root/$IPrecordfile
-bash /usr/local/ddns-check.sh $localport $remoteport $targetDDNS $IPrecordfile $localip &>> /root/iptables${localport}.log
+bash /usr/local/ddns-check-pro.sh $localport $remoteport $targetDDNS $IPrecordfile $localip "$allowed_source" &>> /root/iptables${localport}.log
 echo "done!"
 echo "现在每分钟都会检查ddns的ip是否改变，并自动更新"
